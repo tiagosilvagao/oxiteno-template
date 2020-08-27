853020449c288b4b96f7e67b0601cffef702b373
Igor Andrade <igor.andrade@techne.com.br>
2020-08-27 18:17:57 +0000

    First commit...

---
+++ src/main/mobileapp/www/js/blockly.js
@@ -0,0 +1,15 @@
+window.fixedTimeZone = true;
+window.timeZone = 'UTC';
+window.timeZoneOffset = 0;
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.translations = window.translations || {};
+window.translations.localesKeys = ['en_us','pt_br'];
+window.translations.localesRef =  {"en*":"en_us","*":"pt_br","pt*":"pt_br"};
+document.write("<script src='js/blockly/auth/Login.blockly.js?1598550285306'></script>");
+document.write("<script src='js/blockly/auth/Home.blockly.js?1598550285311'></script>");
+document.write("<script src='js/blockly/auth/ChangePassword.blockly.js?1598550285304'></script>");
+document.write("<script src='plugins/cronapp-framework-mobile-js/components/templates/blockly/auth/ChangePassword.blockly.js?1598550285750'></script>");
+document.write("<script src='plugins/cronapp-framework-mobile-js/components/templates/blockly/auth/Login.blockly.js?1598550285754'></script>");
+document.write("<script src='plugins/cronapp-framework-mobile-js/components/templates/blockly/auth/Home.blockly.js?1598550285752'></script>");
